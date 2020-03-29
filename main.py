23fa843c3f72a51ff4fd9f62886f5ae0d08f336b
Jan-Markus Langer <janmarkuslanger10121994@gmail.com>
2020-03-29 03:03:08 +0200

    Gamesettings

--- main.py
+++ main.py
@@ -4,8 +4,34 @@ import random
 import pygame as pg
 
 main_dir = os.path.split(os.path.abspath(__file__))[0]
-GAME_WIDTH = 500
-GAME_HEIGHT = 500
+GAME_WIDTH = 600
+GAME_HEIGHT = 600
+
+CONFIG = (
+    {
+        'score': 100,
+        'enemie_speed': 1,
+        'enemie_count': 2
+    },
+    {
+        'score': 1000,
+        'enemie_speed': 2,
+        'enemie_count': 3
+    }, {
+        'score': 3000,
+        'enemie_speed': 5,
+        'enemie_count': 3
+    }, {
+        'score': 5000,
+        'enemie_speed': 10,
+        'enemie_count': 4
+    },
+    {
+        'score': 6000,
+        'enemie_speed': 16,
+        'enemie_count': 4
+    }
+)
 
 
 class Element:
@@ -26,13 +52,27 @@ class Element:
     def move_right(self):
         self.pos = self.pos.move(1*self.speed, 0)
 
+    def out_of_range(self):
+        topside = (
+            self.pos.top < 0
+            or self.pos.bottom < 0
+            or self.pos.left < 0
+            or self.pos.right < 0
+        )
+
+        bottomside = (
+            self.pos.top > GAME_HEIGHT
+        )
+
+        return (topside or bottomside)
+
+
 
 class ImageElement(Element):
     def __init__(self, speed, image, top, left):
         super().__init__(speed, top, left)
         self.image = image
         self.pos = self.image.get_rect().move(left, top)
-
 
 # quick function to load an image
 def load_image(name):
@@ -41,12 +81,21 @@ def load_image(name):
 
 # here's the full code
 def main():
+    run = True
     pg.init()
     font = pg.font.Font(None, 25)
     clock = pg.time.Clock()
     screen = pg.display.set_mode((GAME_WIDTH, GAME_HEIGHT))
+    game_area = pg.Rect(0, 0, GAME_WIDTH, GAME_HEIGHT)
     screen.fill((255, 255, 255))
+
+    # setup game vars
     score = 0
+    level = 0
+
+    if (CONFIG[level]['score'] >= score and level < len(CONFIG)):
+        # raise level if score is high enough
+        level += 1
 
     # load background image
     background_image = load_image('office-capitol.png')
@@ -57,7 +106,7 @@ def main():
     player_image = load_image('stromberg.png')
 
     bullet_image = load_image('aktenordner.png')
-    bullet_image = pg.transform.scale(bullet_image, (10, 20))
+    bullet_image = pg.transform.scale(bullet_image, (10, 30))
 
     enemie_images = ['becker.png', 'erika.png', 'frau-nangwasongwa.png', 'tukulu.png']
     for index, img in enumerate(enemie_images):
@@ -76,8 +125,8 @@ def main():
     )
 
 
-    while 1:
-        screen.fill((255, 255, 255))
+    while run:
+        clock.tick(60)
         screen.blit(background_image, (0,0))
 
         text = font.render('Score: ' + str(score), 1, (0,0,0))
@@ -103,11 +152,12 @@ def main():
 
         screen.blit(player.image, player.pos)
 
-        if (len(enemies) <= 2):
+        if (len(enemies) <= CONFIG[level]['enemie_count']):
             enemie_img = enemie_images[random.randint(0, len(enemie_images) - 1)]
+            enemie_speed = CONFIG[level]['enemie_speed']
             enemie = ImageElement(
                 image=enemie_img,
-                speed=1,
+                speed=enemie_speed,
                 top=0,
                 left=random.randint(0, (GAME_WIDTH - enemie_img.get_width()))
             )
@@ -115,6 +165,8 @@ def main():
 
         for enemie in enemies:
             enemie.move_down()
+            # if enemie.out_of_range():
+            #     pass
             screen.blit(enemie.image, enemie.pos)
 
         for bullet in bullets:
@@ -127,9 +179,12 @@ def main():
                     score += 1
 
             screen.blit(bullet.image, bullet.pos)
-
         pg.display.update()
-        clock.tick(30)
+
+        score += 1
+
+    else:
+        print('sdfsd')
 
 
 if __name__ == "__main__":
